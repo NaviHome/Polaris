16dc60ac21ed23afc350f25b3b2ffce948b91a40
PeratX <1215714524@qq.com>
2018-07-10 08:16:40 +0800

    Bump version: 0.2.0 -> new TFT display

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include "module/DHT11.h"
-#include "module/LcdHelper.h"
+//#include "module/LcdHelper.h"
 #include "module/BMP180.h"
 #include "util/Util.h"
 
 const String NAME = "Polaris";
-const String VER = "0.1.0";
+const String VER = "0.2.0";
 
 const int LOOP_DELAY = 500;
 
@@ -16,12 +16,12 @@ void setup()
     pinMode(13, OUTPUT); //led on board
     digitalWrite(13, HIGH);
 
-    LcdHelper::init();
+    /*LcdHelper::init();
     LcdHelper::printFromStart(NAME + " " + VER);
     LcdHelper::printSecondLine("Now loading...");
     delay(LOOP_DELAY * 2);
     LcdHelper::clear();
-    digitalWrite(13, LOW);
+    digitalWrite(13, LOW);*/
 
     BMP180::init();
 }
@@ -29,16 +29,16 @@ void setup()
 void loop()
 {
     long startTime = millis();
-    digitalWrite(13, HIGH); //one loop
+    //digitalWrite(13, HIGH); //one loop
 
-    LcdHelper::printFromStart(String(BMP180::getSensor().readTemperature()) + "C/" + String(BMP180::getSensor().readPressure()) + "Pa");
+    //LcdHelper::printFromStart(String(BMP180::getSensor().readTemperature()) + "C/" + String(BMP180::getSensor().readPressure()) + "Pa");
 
     DHT11::readSensor();
-    LcdHelper::printSecondLine(String(DHT11::getTemperature()) + "C / " + String(DHT11::getHumidity()) + "% " + String(millis() - startTime) + "ms");
+    //LcdHelper::printSecondLine(String(DHT11::getTemperature()) + "C / " + String(DHT11::getHumidity()) + "% " + String(millis() - startTime) + "ms");
 
     Util::encodeAndSendJsonData(NAME, VER);
 
     delay(LOOP_DELAY / 2);
-    digitalWrite(13, LOW);
+    //digitalWrite(13, LOW);
     delay(LOOP_DELAY / 2);
 }
